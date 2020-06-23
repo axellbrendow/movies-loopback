d2f0249bb5d02c3a17b95f38ead6c52c45b0c029
axell-brendow <breno.axel@gmail.com>
2020-06-22 23:39:34 -0300

    Create some bindings

--- src/application.ts
+++ src/application.ts
@@ -7,8 +7,19 @@ import {
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
+import multer from 'multer';
 import path from 'path';
+import {
+  PasswordHasherBindings,
+  StorageServiceBindings,
+  TokenServiceBindings,
+  TokenServiceConstants,
+  UserServiceBindings,
+} from './keys';
 import {MySequence} from './sequence';
+import {BcryptHasher} from './services/hash.password.bcryptjs';
+import {JWTService} from './services/jwt-service';
+import {MyUserService} from './services/user-service';
 
 export {ApplicationConfig};
 
@@ -17,6 +28,8 @@ export class MoviesApplication extends BootMixin(
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
+
+    this.setUpBindings();
 
     // Set up the custom sequence
     this.sequence(MySequence);
@@ -30,6 +43,8 @@ export class MoviesApplication extends BootMixin(
     });
     this.component(RestExplorerComponent);
 
+    this.configureFileUpload(options.fileStorageDirectory);
+
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
@@ -40,5 +55,48 @@ export class MoviesApplication extends BootMixin(
         nested: true,
       },
     };
+  }
+
+  setUpBindings(): void {
+    this.bind(TokenServiceBindings.TOKEN_SECRET).to(
+      TokenServiceConstants.TOKEN_SECRET_VALUE,
+    );
+
+    this.bind(TokenServiceBindings.TOKEN_EXPIRES_IN).to(
+      TokenServiceConstants.TOKEN_EXPIRES_IN_VALUE,
+    );
+
+    this.bind(TokenServiceBindings.TOKEN_SERVICE).toClass(JWTService);
+
+    // // Bind bcrypt hash services
+    this.bind(PasswordHasherBindings.ROUNDS).to(10);
+    this.bind(PasswordHasherBindings.PASSWORD_HASHER).toClass(BcryptHasher);
+
+    this.bind(UserServiceBindings.USER_SERVICE).toClass(MyUserService);
+  }
+
+  /**
+   * Configure `multer` options for file upload
+   */
+  protected configureFileUpload(destination?: string) {
+    destination = destination ?? path.join(__dirname, '../.sandbox');
+
+    this.bind(StorageServiceBindings.STORAGE_DIRECTORY).to(destination);
+
+    const multerOptions: multer.Options = {
+      storage: multer.diskStorage({
+        destination,
+        filename: (req, file, cb) => {
+          cb(
+            null,
+            `${new Date().toISOString().slice(0, 10 + 9)}-${file.originalname}`,
+          );
+        },
+      }),
+    };
+
+    this.configure(StorageServiceBindings.FILE_UPLOAD_SERVICE).to(
+      multerOptions,
+    );
   }
 }
